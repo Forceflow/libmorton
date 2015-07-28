d7ae80b43a6dfef6f7147988805f65c265a6edf0
jerbae@gmail.com <jerbae@gmail.com>
2015-07-28 13:50:38 +0200

    Working on flexible LUT generation, add todo.txt

--- libmorton/include/LUT_generators.h
+++ libmorton/include/LUT_generators.h
@@ -1,13 +1,18 @@
 #include "../../libmorton/include/morton.h"
 
 
-// must be a multiple of three
+// Generate a decode tables for 3D morton code
+// how_many_bits should be a multiple of three
 void generate3DDecodeTables(size_t how_many_bits, uint8_t* x_table, uint8_t* y_table, uint8_t* z_table, bool print_tables){
 	// how many items we going to make need?
 	size_t total = how_many_bits << 2;
 	x_table = (uint8_t*) malloc(total * sizeof(uint8_t));
 	y_table = (uint8_t*) malloc(total * sizeof(uint8_t));
 	z_table = (uint8_t*) malloc(total * sizeof(uint8_t));
+
+	if (print_tables){
+		printf("Generating tables for %llu bits, each table will contain %llu entries at 8 bit per entry. n", how_many_bits, total);
+	}
 
 	//generate 9-bit morton to X table
 	printf("\n X TABLE \n");
