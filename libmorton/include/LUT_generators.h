185d7c2e42423c786e30c4e6592bbd73f0e25fd7
Jeroen Baert <jerbae@gmail.com>
2015-08-20 19:16:54 +0200

    cleaned up some generate tables code

--- libmorton/include/LUT_generators.h
+++ libmorton/include/LUT_generators.h
@@ -1,4 +1,13 @@
 #include "morton3D_64.h"
+#include <iostream>
+
+void generate2D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fast32_t* y_table, uint_fast32_t* z_table, bool print_tables){
+	size_t total = how_many_bits << 2;
+	x_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+	y_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+
+
+}
 
 void generate3D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fast32_t* y_table, uint_fast32_t* z_table, bool print_tables){
 	// how many items
@@ -7,40 +16,36 @@ void generate3D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fas
 
 }
 
+template <typename element>
+void printTable(const element* table, size_t howmany, unsigned int splitat){
+	for (size_t i = 0; i < howmany; i++){
+		if (i % splitat == 0){ cout << endl; }
+		cout << table[i] << ", ";
+	}
+	cout << endl;
+}
+
 // Generate a decode tables for 3D morton code
 // how_many_bits should be a multiple of three
 void generate3D_DecodeLUT(size_t how_many_bits, uint_fast8_t* x_table, uint_fast8_t* y_table, uint_fast8_t* z_table, bool print_tables){
-	// how many items we going to make need?
 	size_t total = how_many_bits << 2;
 	x_table = (uint_fast8_t*) malloc(total * sizeof(uint_fast8_t));
 	y_table = (uint_fast8_t*) malloc(total * sizeof(uint_fast8_t));
 	z_table = (uint_fast8_t*) malloc(total * sizeof(uint_fast8_t));
-	// how many kilobytes will this take?
-	float = total * sizeof(uint_fast8_t)
+
+	//generate tables
+	for (size_t i = 0; i < total; i++){
+		x_table[i] = getThirdBits(i);
+		y_table[i] = getThirdBits(i >> 1);
+		z_table[i] = getThirdBits(i >> 2);
+	}
 
 	if (print_tables){
-		printf("Generating tables for %llu bits, each table will contain %llu entries at 8 bit per entry, totaling to %f kilobyte per table. /n", how_many_bits, total);
-	}
-
-	//generate 9-bit morton to X table
-	printf("\n X TABLE \n");
-	for (size_t i = 0; i < 512; i++){
-		unsigned int result_x = getThirdBits(i);
-		if (i % 16 == 0){ printf("\n"); }
-		printf("%u,", result_x);
-	}
-	//generate 9-bit morton to Y table
-	printf("\n Y TABLE \n");
-	for (size_t i = 0; i < 512; i++){
-		unsigned int result_y = getThirdBits(i >> 1);
-		if (i % 16 == 0){ printf("\n"); }
-		printf("%u,", result_y);
-	}
-	//generate 9-bit morton to Z table
-	printf("\n Z TABLE \n");
-	for (size_t i = 0; i < 512; i++){
-		unsigned int result_z = getThirdBits(i >> 2);
-		if (i % 16 == 0){ printf("\n"); }
-		printf("%u,", result_z);
+		cout << "X Table " << endl;
+		printTable<uint_fast8_t>(x_table, total, 16);
+		cout << "Y Table " << endl;
+		printTable<uint_fast8_t>(x_table, total, 16);
+		cout << "W Table " << endl;
+		printTable<uint_fast8_t>(x_table, total, 16);	
 	}
 }
