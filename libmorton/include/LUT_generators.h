3a9826a4c207fadb5708b809a4c938fcacb1ef02
Jeroen Baert <jerbae@gmail.com>
2015-08-20 19:33:06 +0200

    Finished more table generators shoudl use these in live test as well

--- libmorton/include/LUT_generators.h
+++ libmorton/include/LUT_generators.h
@@ -1,20 +1,6 @@
+#include "morton2D_64.h"
 #include "morton3D_64.h"
 #include <iostream>
-
-void generate2D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fast32_t* y_table, uint_fast32_t* z_table, bool print_tables){
-	size_t total = how_many_bits << 2;
-	x_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
-	y_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
-
-
-}
-
-void generate3D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fast32_t* y_table, uint_fast32_t* z_table, bool print_tables){
-	// how many items
-	size_t total = how_many_bits << 2;
-
-
-}
 
 template <typename element>
 void printTable(const element* table, size_t howmany, unsigned int splitat){
@@ -23,6 +9,50 @@ void printTable(const element* table, size_t howmany, unsigned int splitat){
 		cout << table[i] << ", ";
 	}
 	cout << endl;
+}
+
+void generate2D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fast32_t* y_table, bool print_tables){
+	size_t total = how_many_bits << 2;
+	x_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+	y_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+
+	for (size_t i = 0; i < total; i++){
+		x_table[i] = morton2D_64_Encode_magicbits(i, 0);
+		y_table[i] = morton2D_64_Encode_magicbits(0, i);
+	}
+
+	if (print_tables){
+		cout << "X Table " << endl;
+		printTable<uint_fast32_t>(x_table, total, 8);
+		cout << "Y Table " << endl;
+		printTable<uint_fast32_t>(y_table, total, 8);
+	}
+
+}
+
+void generate3D_EncodeLUT(size_t how_many_bits, uint_fast32_t* x_table, uint_fast32_t* y_table, uint_fast32_t* z_table, bool print_tables){
+	// how many items
+	size_t total = how_many_bits << 2;
+	x_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+	y_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+	z_table = (uint_fast32_t*)malloc(total * sizeof(uint_fast32_t));
+
+	for (size_t i = 0; i < total; i++){
+		x_table[i] = morton3D_64_Encode_magicbits(i, 0, 0);
+		y_table[i] = morton3D_64_Encode_magicbits(0, i, 0);
+		z_table[i] = morton3D_64_Encode_magicbits(0, 0, i);
+	}
+
+	if (print_tables){
+		cout << "X Table " << endl;
+		printTable<uint_fast32_t>(x_table, total, 8);
+		cout << "Y Table " << endl;
+		printTable<uint_fast32_t>(y_table, total, 8);
+		cout << "Z Table " << endl;
+		printTable<uint_fast32_t>(z_table, total, 8);
+	}
+
+
 }
 
 // Generate a decode tables for 3D morton code
