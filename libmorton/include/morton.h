30b5fe6ed7b531fb3a07b2547189c21dd43a2b50
Jeroen Baert <jerbae@gmail.com>
2015-11-28 17:42:54 +0100

    Added other functions to common.h

--- libmorton/include/morton.h
+++ libmorton/include/morton.h
@@ -13,6 +13,11 @@ inline uint_fast64_t morton2D_64_encode(const uint_fast32_t x, const uint_fast32
 inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z);
 inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z);
 
+inline uint_fast32_t morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y);
+inline uint_fast64_t morton2D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y);
+inline uint_fast32_t morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z);
+inline uint_fast64_t morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z);
+
 inline uint_fast32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
 	return morton2D_32_Encode_LUT256_shifted(x, y);
 }
@@ -27,6 +32,23 @@ inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16
 
 inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z){
 	return morton3D_64_Encode_LUT256_shifted(x, y, z);
+}
+
+
+inline uint_fast32_t morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
+	//TODO
+}
+
+inline uint_fast64_t morton2D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y) {
+	//TODO
+}
+
+inline uint_fast32_t morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
+	morton3D_32_Decode_LUT256_shifted(morton, x, y, z);
+}
+
+inline uint_fast64_t morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z) {
+	morton3D_64_Decode_LUT256_shifted(morton, x, y, z);
 }
 
 #endif
