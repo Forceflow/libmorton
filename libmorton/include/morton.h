573f1744f9867be5b0501499eb756177d0c8d8bd
Jeroen Baert <jerbae@gmail.com>
2016-01-24 18:05:30 +0100

    Fixed morton.h

--- libmorton/include/morton.h
+++ libmorton/include/morton.h
@@ -28,11 +28,11 @@ inline uint_fast64_t morton2D_64_encode(const uint_fast32_t x, const uint_fast32
 }
 
 inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
-	return morton3D_32_Encode_LUT256_shifted(x, y, z);
+	return morton3D_Encode_LUT256_shifted<uint_fast32_t, uint_fast16_t>(x, y, z);
 }
 
 inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z){
-	return morton3D_64_Encode_LUT256_shifted(x, y, z);
+	return morton3D_Encode_LUT256_shifted<uint_fast64_t, uint_fast32_t>(x, y, z);
 }
 
 inline void morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
