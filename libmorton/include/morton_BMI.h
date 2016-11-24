58fc2ef0b2c90b1640accff7bf0ce0e48abbc963
Jeroen Baert <jerbae@gmail.com>
2016-11-24 18:53:56 +0100

    BMI2-enabled decode function

--- libmorton/include/morton_BMI.h
+++ libmorton/include/morton_BMI.h
@@ -1,9 +1,7 @@
 #pragma once
-
-#include <stdint.h>
-
 #if defined(__BMI2__)
 #include <immintrin.h>
+#include <stdint.h>
 
 namespace bmi2_detail {
 	inline uint32_t pdep(uint32_t source, uint32_t mask) noexcept {
@@ -20,12 +18,23 @@ namespace bmi2_detail {
 	}
 }  // namespace bmi2_detail
 
+#define BMI_X_MASK 0x9249249249249249
+#define BMI_Y_MASK 0x2492492492492492
+#define BMI_Z_MASK 0x4924924924924924
+
 template<typename morton, typename coord> 
 inline morton m3D_e_BMI(const coord x, const coord y, const coord z){
 	morton m = 0;
-	m |= bmi2_detail::pdep(static_cast<morton>(x), static_cast<morton>(0x9249249249249249))
-	| bmi2_detail::pdep(static_cast<morton>(y), static_cast<morton>(0x2492492492492492))
-	| bmi2_detail::pdep(static_cast<morton>(z), static_cast<morton>(0x4924924924924924));
+	m |=  bmi2_detail::pdep(static_cast<morton>(x), static_cast<morton>(BMI_X_MASK))
+		| bmi2_detail::pdep(static_cast<morton>(y), static_cast<morton>(BMI_Y_MASK))
+		| bmi2_detail::pdep(static_cast<morton>(z), static_cast<morton>(BMI_Z_MASK));
 	return m;
+}
+
+template<typename morton, typename coord>
+inline morton m3d_d_BMI(const morton m, coord& x, coord& y, coord& z) {
+	x = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_X_MASK)));
+	y = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_Y_MASK)));
+	z = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_Z_MASK)));
 }
 #endif
