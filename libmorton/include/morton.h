3ea7773effbf8e9ee94a5f4ecfa7e9d3238e2855
Jeroen Baert <jerbae@gmail.com>
2016-12-04 14:10:06 +0100

    Don't do this with #define, leave it to the compiler

--- libmorton/include/morton.h
+++ libmorton/include/morton.h
@@ -22,6 +22,8 @@
 // Functions under this are stubs which will always point to fastest implementation at the moment
 //-----------------------------------------------------------------------------------------------
 
+// ENCODING
+
 inline uint_fast32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
 	return m2D_e_sLUT<uint_fast32_t, uint_fast16_t>(x, y);
 }
@@ -29,21 +31,43 @@ inline uint_fast64_t morton2D_64_encode(const uint_fast32_t x, const uint_fast32
 	return m2D_e_sLUT<uint_fast64_t, uint_fast32_t>(x, y);
 }
 
-#define morton2D_32_decode m2D_d_sLUT<uint_fast32_t, uint_fast16_t>
-#define morton2D_64_decode m2D_d_sLUT<uint_fast64_t, uint_fast32_t>
-
 #if defined(__BMI2__) || __AVX2__
-#define morton3D_32_encode m3D_e_BMI<uint_fast32_t, uint_fast16_t>
-#define morton3D_64_encode m3D_e_BMI<uint_fast64_t, uint_fast32_t>
+inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
+	return m3D_e_BMI<uint_fast32_t, uint_fast16_t>(x, y, z);
+}
+inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z) {
+	return m3D_e_BMI<uint_fast64_t, uint_fast32_t>(x, y, z);
+}
 #else
-#define morton3D_32_encode m3D_e_sLUT<uint_fast32_t, uint_fast16_t>
-#define morton3D_64_encode m3D_e_sLUT<uint_fast64_t, uint_fast32_t>
+inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z) {
+	return m3D_e_sLUT<uint_fast32_t, uint_fast16_t>(x, y, z);
+}
+inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z) {
+	return m3D_e_sLUT<uint_fast64_t, uint_fast32_t>(x, y, z);
+}
 #endif
 
+// DECODING
+
+inline void morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
+	m2D_d_sLUT<uint_fast32_t, uint_fast16_t>(morton, x, y);
+}
+inline void morton2D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y) {
+	m2D_d_sLUT<uint_fast64_t, uint_fast32_t>(morton, x, y);
+}
+
 #if defined(__BMI2__) || __AVX2__
-#define morton3D_32_decode m3D_d_BMI<uint_fast32_t, uint_fast16_t>
-#define morton3D_64_decode m3D_d_BMI<uint_fast64_t, uint_fast32_t>
+inline void morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
+	m3D_d_BMI<uint_fast32_t, uint_fast16_t>(morton, x, y, z);
+}
+inline void morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z) {
+	m3D_d_BMI<uint_fast64_t, uint_fast32_t>(morton, x, y, z);
+}
 #else
-#define morton3D_32_decode m3D_d_sLUT<uint_fast32_t, uint_fast16_t>
-#define morton3D_64_decode m3D_d_sLUT<uint_fast64_t, uint_fast32_t>
+inline void morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z) {
+	m3D_d_sLUT<uint_fast32_t, uint_fast16_t>(morton, x, y, z);
+}
+inline void morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z) {
+	m3D_d_sLUT<uint_fast64_t, uint_fast32_t>(morton, x, y, z);
+}
 #endif
