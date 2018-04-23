6b50f3f274f69f423f9881980fd05c98c7cfce74
Jeroen Baert <jerbae@gmail.com>
2018-04-23 04:13:46 +0200

    Cleanup main include header

--- libmorton/include/morton.h
+++ libmorton/include/morton.h
@@ -1,23 +1,14 @@
 #pragma once
 
-// This file will always contain #define clauses to the fastest Morton encoding/decoding implementation
+// This file will always contain inline functions which point to the fastest Morton encoding/decoding implementation
 // IF you just want to use the fastest method to encode/decode morton codes, include this header.
+
+// If you want to experiment with alternative methods (which might be slower, all depending on hardware / your data set)
+// check the individual headers below.
 
 #include "morton2D.h"
 #include "morton3D.h"
 #include "morton_BMI.h"
-
-//// ENCODE
-//inline uint_fast32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y);
-//inline uint_fast64_t morton2D_64_encode(const uint_fast32_t x, const uint_fast32_t y);
-//inline uint_fast32_t morton3D_32_encode(const uint_fast16_t x, const uint_fast16_t y, const uint_fast16_t z);
-//inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z);
-//
-//// DECODE
-//inline void morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y);
-//inline void morton2D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y);
-//inline void morton3D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y, uint_fast16_t& z);
-//inline void morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z);
 
 // Functions under this are stubs which will always point to fastest implementation at the moment
 //-----------------------------------------------------------------------------------------------
