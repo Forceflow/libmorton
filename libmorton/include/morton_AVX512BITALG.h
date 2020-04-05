71718d51863872d2587230b2d11dbdfc1ce5012f
Wunkolo <Wunkolo@gmail.com>
2020-04-04 22:33:47 -0700

    First pass AVX512BITALG+AVX512VBMI encoding
    Implements fully implements encoding for 2D and 3D cases.
    Not particular optimized but passes all the tests.
    Uses BMI2 placeholder implementation for decoding for verification
    
    Current comparison against BMI2
    
    ```
    
    CPU:       Topology: Quad Core model: Intel Core i7-1065G7 bits: 64 type: MT MCP L2 cache: 8192 KiB
               Speed: 834 MHz min/max: 400/3900 MHz Core speeds (MHz): 1: 969 2: 1018 3: 923 4: 1037 5: 708 6: 992 7: 613 8: 691
    
        02.050 ms 1.818 ms : 64-bit BMI2 instruction set
        04.213 ms 4.208 ms : 64-bit AVX512 instruction set
        1.835 ms 1.803 ms : 32-bit BMI2 instruction set
        4.222 ms 4.216 ms : 32-bit AVX512 instruction set
        2.021 ms 5.489 ms : 64-bit BMI2 Instruction set
        1.952 ms 5.462 ms : 64-bit AVX512 Instruction set
        01.964 ms 5.285 ms : 32-bit BMI2 Instruction set
        01.964 ms 5.517 ms : 32-bit AVX512 Instruction set
        14.823 ms 14.787 ms : 64-bit BMI2 instruction set
        33.683 ms 34.622 ms : 64-bit AVX512 instruction set
        14.487 ms 14.675 ms : 32-bit BMI2 instruction set
        33.629 ms 33.935 ms : 32-bit AVX512 instruction set
        15.448 ms 41.621 ms : 64-bit BMI2 Instruction set
        15.282 ms 43.666 ms : 64-bit AVX512 Instruction set
        15.635 ms 42.059 ms : 32-bit BMI2 Instruction set
        16.512 ms 44.433 ms : 32-bit AVX512 Instruction set
        137.746 ms 135.549 ms : 64-bit BMI2 instruction set
        314.947 ms 325.968 ms : 64-bit AVX512 instruction set
        136.730 ms 132.937 ms : 32-bit BMI2 instruction set
        315.256 ms 321.689 ms : 32-bit AVX512 instruction set
        141.504 ms 374.482 ms : 64-bit BMI2 Instruction set
        136.387 ms 381.539 ms : 64-bit AVX512 Instruction set
        153.533 ms 370.826 ms : 32-bit BMI2 Instruction set
        136.177 ms 377.794 ms : 32-bit AVX512 Instruction set
    ```

--- libmorton/include/morton_AVX512BITALG.h
+++ libmorton/include/morton_AVX512BITALG.h
@@ -1,5 +1,4 @@
 #pragma once
-#if defined(__AVX512BITALG__)
 #include <immintrin.h>
 #include <stdint.h>
 
@@ -9,31 +8,29 @@ namespace libmorton {
 		// "Zip" and interleave an m-vector of n-bit integers into a
 		// new n*m-bit integer
 		// 2D MORTONS
-		// zip two 16-bit coords into a 32-bit morton
-		inline uint32_t bitzip(uint16_t a, uint16_t b) noexcept {
-			// Put both 16-bit integers into each 32-bit lane
-			const __m256i CoordVec = _mm256_set1_epi32(
-				(static_cast<uint32_t>(b) << 16u) | a
-			);
-			// Interleave bits from 32-bit X and Y coordinate
-			const __mmask32 Interleave = _mm256_bitshuffle_epi64_mask(
-				CoordVec,
-				_mm256_set_epi16(
-					0x1000 + 0x0101 * 15, 0x1000 + 0x0101 * 14,
-					0x1000 + 0x0101 * 13, 0x1000 + 0x0101 * 12,
-					0x1000 + 0x0101 * 11, 0x1000 + 0x0101 * 10,
-					0x1000 + 0x0101 *  9, 0x1000 + 0x0101 *  8,
-					0x1000 + 0x0101 *  7, 0x1000 + 0x0101 *  6,
-					0x1000 + 0x0101 *  5, 0x1000 + 0x0101 *  4,
-					0x1000 + 0x0101 *  3, 0x1000 + 0x0101 *  2,
-					0x1000 + 0x0101 *  1, 0x1000 + 0x0101 *  0
-				)
-			);
-			return _cvtmask32_u32(Interleave);
-		}
-		
-		// zip two 32-bit coords into a 64-bit morton
-		inline uint64_t bitzip(uint32_t a, uint32_t b) noexcept {
+		// inline uint16_t bitzip2D(uint16_t a, uint16_t b) noexcept {
+		// 	// Put both 16-bit integers into each 32-bit lane
+		// 	const __m256i CoordVec = _mm256_set1_epi32(
+		// 		(static_cast<uint32_t>(b) << 16u) | a
+		// 	);
+		// 	// Interleave bits from 32-bit X and Y coordinate
+		// 	const __mmask32 Interleave = _mm256_bitshuffle_epi64_mask(
+		// 		CoordVec,
+		// 		_mm256_set_epi16(
+		// 			0x1000 + 0x0101 * 15, 0x1000 + 0x0101 * 14,
+		// 			0x1000 + 0x0101 * 13, 0x1000 + 0x0101 * 12,
+		// 			0x1000 + 0x0101 * 11, 0x1000 + 0x0101 * 10,
+		// 			0x1000 + 0x0101 *  9, 0x1000 + 0x0101 *  8,
+		// 			0x1000 + 0x0101 *  7, 0x1000 + 0x0101 *  6,
+		// 			0x1000 + 0x0101 *  5, 0x1000 + 0x0101 *  4,
+		// 			0x1000 + 0x0101 *  3, 0x1000 + 0x0101 *  2,
+		// 			0x1000 + 0x0101 *  1, 0x1000 + 0x0101 *  0
+		// 		)
+		// 	);
+		// 	return _cvtmask32_u32(Interleave);
+		// }
+
+		inline uint64_t bitzip2D(uint64_t a, uint64_t b) noexcept {
 			// Put both 32-bit integers into each 64-bit lane
 			const __m512i CoordVec = _mm512_set1_epi64(
 				(static_cast<uint64_t>(b) << 32u) | a
@@ -62,84 +59,108 @@ namespace libmorton {
 			);
 			return _cvtmask64_u64(Interleave);
 		}
-		// 3D MORTONS
-		// zip three 16-bit coords into a 32-bit morton
-		inline uint32_t bitzip(uint16_t a, uint16_t b, uint16_t c) noexcept {
-			// Put both 32-bit integers into each 64-bit lane
-			const __m256i CoordVec = _mm256_set1_epi64x(
-				static_cast<std::uint64_t>(a)
-				| static_cast<std::uint64_t>(b << 16)
-				| static_cast<std::uint64_t>(c << 32)
+		inline uint32_t bitzip2D(uint32_t a, uint32_t b) noexcept {
+			return bitzip2D(
+				static_cast<uint64_t>(a),
+				static_cast<uint64_t>(b)
 			);
-			// Interleave bits from 16-bit X and Y and Z coordinate
-			const __mmask32 Interleave = _mm256_bitshuffle_epi64_mask(
-				CoordVec,
-				_mm256_set_epi64x(
-					0x1A'0A'29'19'09'28'18'08, // Byte 3
-					0x27'17'07'26'16'06'25'15, // Byte 2
-					0x05'24'14'04'23'13'03'22, // Byte 1
-					0x12'02'21'11'01'20'10'00  // Byte 0
-				)
-			);
-			return _cvtmask32_u32(Interleave));
 		}
-		// zip three 32-bit coords into a 64-bit morton
-		inline uint64_t bitzip(uint32_t a, uint32_t b, uint32_t c) noexcept {
+		// 3D MORTONS
+		// inline uint16_t bitzip3D(uint16_t a, uint16_t b, uint16_t c) noexcept {
+		// 	// Put both 32-bit integers into each 64-bit lane
+		// 	const __m256i CoordVec = _mm256_set1_epi64x(
+		// 		static_cast<std::uint64_t>(a)
+		// 		| (static_cast<std::uint64_t>(b) << 16)
+		// 		| (static_cast<std::uint64_t>(c) << 32)
+		// 	);
+		// 	// Interleave bits from 16-bit X and Y and Z coordinate
+		// 	const __mmask32 Interleave = _mm256_bitshuffle_epi64_mask(
+		// 		CoordVec,
+		// 		_mm256_set_epi64x(
+		// 			0x1A0A291909281808, // Byte 3
+		// 			0x2717072616062515, // Byte 2
+		// 			0x0524140423130322, // Byte 1
+		// 			0x1202211101201000  // Byte 0
+		// 		)
+		// 	);
+		// 	return _cvtmask32_u32(Interleave);
+		// }
+		inline uint64_t bitzip3D(uint64_t a, uint64_t b, uint64_t c) noexcept {
 			// Put both 32-bit integers into each 64-bit lane
-			const __m512i CoordVec = _mm512_broadcast_i32x4(
-				_mm_set_epi32(0, c, b, a)
+			// Todo: _mm512_shuffle_epi8 version, 128-bit lane woes
+			const __m512i CoordVec = _mm512_set_epi64(
+				0, 0, 0, 0, 0, c, b, a
 			);
-			const __m512i ShuffleVec = _mm512_shuffle_epi8(
-				CoordVec,
+			const __m512i ShuffleVec = _mm512_permutexvar_epi8(
 				_mm512_set_epi64(
-					0x0A'06'02ul, // Lane 7 | z2 | y2 | x2
-					0x0A'06'02ul, // Lane 6 | z2 | y2 | x2
-					0x09'05'01ul, // Lane 5 | z1 | y1 | x1
-					0x09'05'01ul, // Lane 4 | z1 | y1 | x1
-					0x09'05'01ul, // Lane 3 | z1 | y1 | x1
-					0x08'04'00ul, // Lane 2 | z0 | y0 | x0
-					0x08'04'00ul, // Lane 1 | z0 | y0 | x0
-					0x08'04'00ul  // Lane 0 | z0 | y0 | x0
-				)
+					0xFFFFFFFFFF100800ul + 0x010101 * 2, // Lane 7 | ...000 | z[2] | y[2] | x[2]
+					0xFFFFFFFFFF100800ul + 0x010101 * 2, // Lane 6 | ...000 | z[2] | y[2] | x[2]
+					0xFFFFFFFFFF100800ul + 0x010101 * 1, // Lane 5 | ...000 | z[1] | y[1] | x[1]
+					0xFFFFFFFFFF100800ul + 0x010101 * 1, // Lane 4 | ...000 | z[1] | y[1] | x[1]
+					0xFFFFFFFFFF100800ul + 0x010101 * 1, // Lane 3 | ...000 | z[1] | y[1] | x[1]
+					0xFFFFFFFFFF100800ul + 0x010101 * 0, // Lane 2 | ...000 | z[0] | y[0] | x[0]
+					0xFFFFFFFFFF100800ul + 0x010101 * 0, // Lane 1 | ...000 | z[0] | y[0] | x[0]
+					0xFFFFFFFFFF100800ul + 0x010101 * 0  // Lane 0 | ...000 | z[0] | y[0] | x[0]
+				),
+				CoordVec
+			);
+			const __m512i BitIndex =
+			_mm512_set_epi64(
+				0x0504040403030302 + 0x0002010002010002 * 8,
+				0x0202010101000000 + 0x0100020100020100 * 8,
+				0x0707070606060505 + 0x0201000201000201 * 8,
+				0x0504040403030302 + 0x0002010002010002 * 8,
+				0x0202010101000000 + 0x0100020100020100 * 8,
+				0x0707070606060505 + 0x0201000201000201 * 8,
+				0x0504040403030302 + 0x0002010002010002 * 8,
+				0x0202010101000000 + 0x0100020100020100 * 8 
 			);
 			// Interleave bits from 32-bit X and Y and Z coordinate
 			const __mmask64 Interleave = _mm512_bitshuffle_epi64_mask(
-				ShuffleVec,
-				_mm512_set_epi64(
-					0x15'14'14'14'13'13'13'12, // Byte 7
-					0x12'12'11'11'11'10'10'10, // Byte 6
-					0x0F'0F'0F'0E'0E'0E'0D'0D, // Byte 5
-					0x0D'0C'0C'0C'0B'0B'0B'0A, // Byte 4
-					0x0A'0A'09'09'09'08'08'08, // Byte 3
-					0x07'07'07'06'06'06'05'05, // Byte 2
-					0x05'04'04'04'03'03'03'02, // Byte 1
-					0x02'02'01'01'01'00'00'00  // Byte 0
-				)
+				ShuffleVec, BitIndex
 			);
 			return _cvtmask64_u64(Interleave);
+		}
+		inline uint32_t bitzip3D(uint32_t a, uint32_t b, uint32_t c) noexcept {
+			return bitzip3D(
+				static_cast<uint64_t>(a),
+				static_cast<uint64_t>(b),
+				static_cast<uint64_t>(c)
+			);
 		}
 	}  // namespace bitalg_detail
 
 	template<typename morton, typename coord>
 	inline morton m2D_e_BITALG(const coord x, const coord y) {
-		return bitalg_detail::bitzip(
+		return bitalg_detail::bitzip2D(
 			static_cast<coord>(x), static_cast<coord>(y)
 		);
 	}
 
 	template<typename morton, typename coord>
 	inline void m2D_d_BITALG(const morton m, coord& x, coord& y) {
+		// Placeholder BMI implementation for verification
+		#define BMI_2D_X_MASK 0x5555555555555555
+		#define BMI_2D_Y_MASK 0xAAAAAAAAAAAAAAAA
+		x = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_3D_X_MASK)));
+		y = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_3D_Y_MASK)));
 	}
 
 	template<typename morton, typename coord>
 	inline morton m3D_e_BITALG(const coord x, const coord y, const coord z) {
-		return bitalg_detail::bitzip(
+		return bitalg_detail::bitzip3D(
 			static_cast<coord>(x), static_cast<coord>(y),  static_cast<coord>(z)
 		);
 	}
 
 	template<typename morton, typename coord>
 	inline void m3D_d_BITALG(const morton m, coord& x, coord& y, coord& z) {
+		// Placeholder BMI implementation for verification
+		#define BMI_3D_X_MASK 0x9249249249249249
+		#define BMI_3D_Y_MASK 0x2492492492492492
+		#define BMI_3D_Z_MASK 0x4924924924924924
+		x = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_3D_X_MASK)));
+		y = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_3D_Y_MASK)));
+		z = static_cast<coord>(bmi2_detail::pext(m, static_cast<morton>(BMI_3D_Z_MASK)));
 	}
 }
-#endif
