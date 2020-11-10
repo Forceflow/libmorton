d7244f8bfa7865d4f1e47bb2a9ff2f81df91827b
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2020-11-10 23:52:47 +0100

    Additional #ifdef fixes

--- libmorton/include/morton.h
+++ libmorton/include/morton.h
@@ -9,12 +9,10 @@
 #include "morton2D.h"
 #include "morton3D.h"
 
-#if defined(__BMI2__) || (defined(__AVX2__) && defined(_MSC_VER))
-#include "morton_BMI.h"
-#endif
-
 #if defined(__AVX512BITALG__)
 #include "morton_AVX512BITALG.h"
+#elif defined(__BMI2__) || (defined(__AVX2__) && defined(_MSC_VER))
+#include "morton_BMI.h"
 #endif
 
 namespace libmorton {
@@ -35,7 +33,7 @@ namespace libmorton {
 	inline uint_fast64_t morton3D_64_encode(const uint_fast32_t x, const uint_fast32_t y, const uint_fast32_t z) {
 		return m3D_e_BITALG<uint_fast64_t, uint_fast32_t>(x, y, z);
 	}
-#elif defined(__BMI2__) || defined(__AVX2__)
+#elif defined(__BMI2__) || (defined(__AVX2__) && defined(_MSC_VER))
 	inline uint_fast32_t morton2D_32_encode(const uint_fast16_t x, const uint_fast16_t y) {
 		return m2D_e_BMI<uint_fast32_t, uint_fast16_t>(x, y);
 	}
@@ -77,7 +75,7 @@ namespace libmorton {
 	inline void morton3D_64_decode(const uint_fast64_t morton, uint_fast32_t& x, uint_fast32_t& y, uint_fast32_t& z) {
 		m3D_d_BITALG<uint_fast64_t, uint_fast32_t>(morton, x, y, z);
 	}
-#elif defined(__BMI2__) || defined(__AVX2__)
+#elif defined(__BMI2__) || (defined(__AVX2__) && defined(_MSC_VER))
 	inline void morton2D_32_decode(const uint_fast32_t morton, uint_fast16_t& x, uint_fast16_t& y) {
 		m2D_d_BMI<uint_fast32_t, uint_fast16_t>(morton, x, y);
 	}
